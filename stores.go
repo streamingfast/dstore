7ef0af28e3eab4995e440b5120039150ecee5c71
Matthieu Vachon <matt@dfuse.io>
2020-08-20 10:12:55 -0400

    Added Store2 interface RFC

--- stores.go
+++ stores.go
@@ -2,10 +2,12 @@ package dstore
 
 import (
 	"bufio"
+	"bytes"
 	"context"
 	"errors"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/url"
 	"os"
 	"strings"
@@ -27,6 +29,81 @@ type Store interface {
 
 	DeleteObject(ctx context.Context, base string) error
 }
+
+// Proposal: New interfaces for dstore
+
+type walkQuery struct {
+	prefix       string
+	ignoreSuffix string
+	max          int
+}
+
+type WalkOption interface {
+	apply(query *walkQuery)
+}
+
+type PrefixedWith string
+
+func (o PrefixedWith) apply(query *walkQuery) {
+	query.prefix = string(o)
+}
+
+type IgnoreSuffixedWith string
+
+func (o IgnoreSuffixedWith) apply(query *walkQuery) {
+	query.ignoreSuffix = string(o)
+}
+
+type Limit int
+
+func (o Limit) apply(query *walkQuery) {
+	query.max = int(o)
+}
+
+type Store2 interface {
+	Exists(ctx context.Context, name string) (bool, error)
+	Delete(ctx context.Context, name string) error
+
+	Reader(ctx context.Context, name string) (out io.ReadCloser, err error)
+	Writer(ctx context.Context, name string) (out io.WriteCloser, err error)
+
+	Walk(ctx context.Context, f func(filename string) (err error), options ...WalkOption) error
+}
+
+// Now helper functions (or still straight in the interface and we exposed a "BaseStore" that every implementation
+// can embedded to reduce duplication). I kind of prefer the embedding for easier discovery, for example completion
+// on a store instance will list all available methods straight. But this clutter the interface at the same time could
+// be benefical for some specific implementation so they can provided "fast" path.
+func ListFiles(ctx context.Context, store Store2, options ...WalkOption) ([]string, error) {
+	panic("not implemented")
+}
+
+func ReadBytes(ctx context.Context, store Store2, name string) ([]byte, error) {
+	reader, err := store.Reader(ctx, name)
+	if err != nil {
+		return nil, fmt.Errorf("read: %w", err)
+	}
+	defer reader.Close()
+
+	return ioutil.ReadAll(reader)
+}
+
+func Write(ctx context.Context, store Store2, name string, reader io.Reader) error {
+	writer, err := store.Writer(ctx, name)
+	if err != nil {
+		return fmt.Errorf("writer: %w", err)
+	}
+	defer writer.Close()
+
+	_, err = io.Copy(writer, reader)
+	return err
+}
+
+func WriteBytes(ctx context.Context, store Store2, name string, content []byte) error {
+	return Write(ctx, store, name, bytes.NewBuffer(content))
+}
+
+// End Proposals: New interfaces for dstore
 
 var StopIteration = errors.New("stop iteration")
 
