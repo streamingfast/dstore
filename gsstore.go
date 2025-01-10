8a26a68fb6115f727a06157f284416a2d5562b9d
gldeng <realgldeng@gmail.com>
2025-01-10 23:34:47 +0800

    fix: ignore baseName == "" where gcp bucket returns the directory name as the first item when listing the directory

--- gsstore.go
+++ gsstore.go
@@ -288,11 +288,14 @@ func (s *GSStore) WalkFrom(ctx context.Context, prefix, startingPoint string, f 
 		if err != nil {
 			return err
 		}
-		if err := f(s.toBaseName(attrs.Name)); err != nil {
-			if errors.Is(err, StopIteration) {
-				return nil
+		baseName := s.toBaseName(attrs.Name)
+		if baseName != "" {
+			if err := f(baseName); err != nil {
+				if errors.Is(err, StopIteration) {
+					return nil
+				}
+				return err
 			}
-			return err
 		}
 	}
 	return nil
